02431357a13a2f7b436940867c17cbe93747c305
Maximilian <scientificresearch309@gmail.com>
2022-11-08 22:08:56 +0100

    addeventlistener-Enter--Click

--- main.js
+++ main.js
@@ -1,21 +1,41 @@
 // console.log("Javascript Works!");
 const messageElem = document.querySelector(".message");
 const changeTextButtonElem = document.querySelector(".changeTextButton");
+const userTextElem = document.querySelector(".userText");
 
 console.log(messageElem);
 
 messageElem.innerText = "Initial Text";
 
+const changeText = () => {
+  messageElem.innerText = "The Button was clicked! ";
+};
+
 // changeTextButtonElem.addEventListener("click", () => {
-//   console.log("button was clicked");
+//   //   console.log("button was clicked");
 //   //   messageElem.innerHTML = "the <b>button</b> was clicked";
 //   messageElem.innerText = "the button was clicked";
 // });
+
+changeTextButtonElem.addEventListener("click", changeText);
+//   console.log("button was clicked");
+//   messageElem.innerHTML = "the <b>button</b> was clicked";
+//   messageElem.innerText = "the button was clicked";
 
 // const test = () =>{
 //     console.log('Works');
 // }
 
-const changeText = () => {
-  messageElem.innerText = "The Button was clicked!";
-};
+userTextElem.addEventListener("keypress", (e) => {
+  if (e.keyCode === 13) {
+    // messageElem.innerText = "Enter Key was pressed";
+    changeText();
+    // messageElem.innerText = userTextElem.value;
+    // console.log("Enter Key was pressed!");
+    // console.log(e);
+  }
+  //   messageElem.innerText = "Key up was pressed";
+  //   messageElem.innerText = userTextElem.value;
+  //   console.log("Keyup was pressed.");
+  //   console.log(e);
+});

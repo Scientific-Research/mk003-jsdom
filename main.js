bfa6fcd6178791b0788383cf0129eb44e029142e
Maximilian <scientificresearch309@gmail.com>
2022-11-08 23:01:59 +0100

    jumps to next field ok

--- main.js
+++ main.js
@@ -14,11 +14,12 @@ console.log(message1Elem);
 
 // message1Elem.innerText = "Initial Text";
 
-const changeText = () => {
+// One
+const changeText1 = () => {
   //   messageElem.innerText = "The Button was clicked! ";
   message1Elem.innerText = userText1Elem.value;
   userText1Elem.value = "";
-  userText1Elem.focus();
+  userText2Elem.focus();
 };
 
 // changeTextButtonElem.addEventListener("click", () => {
@@ -27,7 +28,7 @@ const changeText = () => {
 //   messageElem.innerText = "the button was clicked";
 // });
 
-changeText1ButtonElem.addEventListener("click", changeText);
+changeText1ButtonElem.addEventListener("click", changeText1);
 //   console.log("button was clicked");
 //   messageElem.innerHTML = "the <b>button</b> was clicked";
 //   messageElem.innerText = "the button was clicked";
@@ -39,7 +40,44 @@ changeText1ButtonElem.addEventListener("click", changeText);
 userText1Elem.addEventListener("keypress", (e) => {
   if (e.keyCode === 13) {
     // messageElem.innerText = "Enter Key was pressed";
-    changeText();
+    changeText1();
+    // messageElem.innerText = userTextElem.value;
+    // console.log("Enter Key was pressed!");
+    // console.log(e);
+  }
+  //   messageElem.innerText = "Key up was pressed";
+  //   messageElem.innerText = userTextElem.value;
+  //   console.log("Keyup was pressed.");
+  //   console.log(e);
+});
+
+// Two
+const changeText2 = () => {
+  //   messageElem.innerText = "The Button was clicked! ";
+  message2Elem.innerText = userText2Elem.value;
+  userText2Elem.value = "";
+  userText1Elem.focus();
+};
+
+// changeTextButtonElem.addEventListener("click", () => {
+//   //   console.log("button was clicked");
+//   //   messageElem.innerHTML = "the <b>button</b> was clicked";
+//   messageElem.innerText = "the button was clicked";
+// });
+
+changeText2ButtonElem.addEventListener("click", changeText2);
+//   console.log("button was clicked");
+//   messageElem.innerHTML = "the <b>button</b> was clicked";
+//   messageElem.innerText = "the button was clicked";
+
+// const test = () =>{
+//     console.log('Works');
+// }
+
+userText2Elem.addEventListener("keypress", (e) => {
+  if (e.keyCode === 13) {
+    // messageElem.innerText = "Enter Key was pressed";
+    changeText2();
     // messageElem.innerText = userTextElem.value;
     // console.log("Enter Key was pressed!");
     // console.log(e);

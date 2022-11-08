da28975d6ae32ed3a6453fe83ff2a9cae43e286a
Maximilian <scientificresearch309@gmail.com>
2022-11-08 20:07:00 +0100

    innerText && innerHTML

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 // console.log("Javascript Works!");
 const messageElem = document.querySelector(".message");
+const changeTextButtonElem = document.querySelector(".changeTextButton");
+
 console.log(messageElem);
 
-messageElem.innerHTML = "kjhdk"
+messageElem.innerText = "Initial Text";
+
+changeTextButtonElem.addEventListener("click", () => {
+  console.log("button was clicked");
+  //   messageElem.innerHTML = "the <b>button</b> was clicked";
+  messageElem.innerText = "the button was clicked";
+});
